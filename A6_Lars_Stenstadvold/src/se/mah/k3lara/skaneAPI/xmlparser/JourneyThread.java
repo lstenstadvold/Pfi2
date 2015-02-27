285128bdd7a8e457c8347e5f99e991992266d54a
lstenstadvold <lstenstadvold@gmail.com>
2015-02-27 13:06:04 +0100

    I think I've figured out the threads. Ready to redovisa this afternoon.

--- A6_Lars_Stenstadvold/src/se/mah/k3lara/skaneAPI/xmlparser/JourneyThread.java
+++ A6_Lars_Stenstadvold/src/se/mah/k3lara/skaneAPI/xmlparser/JourneyThread.java
@@ -24,6 +24,7 @@ public class JourneyThread extends Thread {
 		for (Journey journey : journeys.getJourneys()) {
 			gui.textAreaJourneys.append(journey.getStartStation()+" - " + journey.getEndStation());
 			String time = journey.getDepDateTime().get(Calendar.HOUR_OF_DAY)+":"+journey.getDepDateTime().get(Calendar.MINUTE);
+			
 			gui.textAreaJourneys.append(" Departs " + time +" that is in "+journey.getTimeToDeparture()+ " minutes. And it is "+journey.getDepTimeDeviation()+" min late\n");
 			gui.textAreaJourneys.append("Arrival time: " + journey.getArrDateTime().get(Calendar.HOUR_OF_DAY) +":"+journey.getArrDateTime().get(Calendar.MINUTE) + "\n");
 			gui.textAreaJourneys.append("Nr of changes: " + journey.getNoOfChanges() + "\n");
